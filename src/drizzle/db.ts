115dab462a676841a45375fff56eba23d01d527e
ifan3735 <ifanmaina@gmail.com>
2024-06-14 17:34:58 +0300

    commit

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema"
+import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from './schema';
 
-export const client = neon (process.env.Database_URL!)  //get the database url from the environment
+const databaseUrl = process.env.DATABASE_URL as string
+if (!databaseUrl) throw new Error("DATABASE_URL is not set");
 
+const sql = neon(databaseUrl);
 
-
-const db = drizzle(client, { schema, logger: true })  //create a drizzle instance
-
-export default db;
+export const db: NeonHttpDatabase<typeof schema> = drizzle(sql, { schema, logger: true });
